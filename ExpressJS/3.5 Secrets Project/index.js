43d3634f9d4bca2e7944193e790715789a416991
vanwani2000 <rajavanwani2001@gmail.com>
2024-07-11 21:26:42 +0530

    Changes In Express

---
+++ ExpressJS/3.5 Secrets Project/index.js
@@ -0,0 +1,33 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    // If password is correct then send the secret.html file.
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    // If the password is not correct then send the index.html file
+    res.sendFile(__dirname + "/public/index.html");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
