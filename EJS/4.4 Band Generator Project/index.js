78f37ff0f617a42c6ffb8225a12ab556753f1d64
vanwani2000 <rajavanwani2001@gmail.com>
2024-09-04 02:24:12 -0700

    Add the files.

--- EJS/4.4 Band Generator Project/index.js
+++ EJS/4.4 Band Generator Project/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { renderFile } from "ejs";
 
 const app = express();
 const port = 3000;
@@ -27,12 +28,12 @@ app.post("/submit", (req, res) => {
   //scroll down to see the two arrays.
   //2. Send the index.ejs as a response and add the adjective and noun to the res.render
   //3. Test to make sure that the random words display in the h1 element in index.ejs
-  const fName = adj[Math.floor(Math.random() * adj.length)];  
+  const fName = adj[Math.floor(Math.random() * adj.length)];
   const lName = noun[Math.floor(Math.random() * noun.length)];
   const full_name = fName + " " + lName;
   res.render("index.ejs", {
     fullName: full_name,
-  })
+  });
 });
 
 app.listen(port, () => {
